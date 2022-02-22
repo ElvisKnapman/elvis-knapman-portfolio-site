e21d4918b26a20c87117df319b962768360a8f57
Elvis Knapman <46550724+ElvisKnapman@users.noreply.github.com>
2022-02-22 18:18:13 -0500

    update build command in package.json...
    add netlify form information to contact component

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -1,40 +1,50 @@
-import styles from './Contact.module.scss';
-import { IoIosSend } from 'react-icons/io';
+import styles from "./Contact.module.scss";
+import { IoIosSend } from "react-icons/io";
 const Contact = () => {
   return (
     <section
-      id='contact'
-      className={`section-container bg-secondary ${styles['section-text']}`}>
-      <div className='container'>
-        <h2 className='section-heading'>Contact</h2>
-        <div className={styles['contact-flex-container']}>
-          <div className={styles['contact-info']}>
+      id="contact"
+      className={`section-container bg-secondary ${styles["section-text"]}`}>
+      <div className="container">
+        <h2 className="section-heading">Contact</h2>
+        <div className={styles["contact-flex-container"]}>
+          <div className={styles["contact-info"]}>
             {/* <h3 className={styles['contact-info-heading']}>Let&apos;s talk!</h3> */}
-            <p className={styles['form-intro-text']}>
+            <p className={styles["form-intro-text"]}>
               Want to talk about a new opportunity? Life? Your favourite TV
               Show? How my love for hockey makes me TOO Canadian?
             </p>
           </div>
-          <div className={styles['contact-form']}>
-            <form className={styles['form']}>
-              <h3 className={styles['form-heading']}>Send a message</h3>
-              <label htmlFor='name'>Name</label>
-              <input type='text' id='name' />
+          <div className={styles["contact-form"]}>
+            <form
+              method="POST"
+              className={styles["form"]}
+              name="portfolio-site-contact"
+              data-netlify="true"
+              netlify-honeypot="bot-field">
+              <h3 className={styles["form-heading"]}>Send a message</h3>
+              <input
+                type="hidden"
+                name="form-name"
+                value="portfolio-site-contact"
+              />
+              <label htmlFor="name">Name</label>
+              <input type="text" id="name" />
 
-              <label htmlFor='email'>Email</label>
-              <input type='email' id='email' />
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" />
 
-              <label htmlFor='message'>Message</label>
+              <label htmlFor="message">Message</label>
               <textarea
-                name='message'
-                id='message'
+                name="message"
+                id="message"
                 cols={5}
                 rows={5}></textarea>
-              <button className={styles['submit-button']} type='submit'>
-                Send{' '}
+              <button className={styles["submit-button"]} type="submit">
+                Send{" "}
                 <IoIosSend
-                  className={styles['email-icon']}
-                  style={{ fontSize: '2rem' }}
+                  className={styles["email-icon"]}
+                  style={{ fontSize: "2rem" }}
                 />
               </button>
             </form>
